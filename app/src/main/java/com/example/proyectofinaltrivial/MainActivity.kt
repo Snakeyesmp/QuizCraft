633ab614b9190197a54fa0bc75b8c122cf426c6e
David330DVD <davidarcones@hotmail.com>
2023-12-29 15:31:32 +0100

    Bloque si no tiene conexion

--- app/src/main/java/com/example/proyectofinaltrivial/MainActivity.kt
+++ app/src/main/java/com/example/proyectofinaltrivial/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.proyectofinaltrivial
 
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.net.NetworkInfo
@@ -13,6 +16,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private var connectivityReceiver: ConnectivityReceiver? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,36 +32,43 @@ class MainActivity : AppCompatActivity() {
             .replace(R.id.fragmentJuego, JuegoFragment())
             .commit()
 
+        connectivityReceiver = ConnectivityReceiver()
+        val intentFilter = IntentFilter("android.net.conn.CONNECTIVITY_CHANGE")
+        registerReceiver(connectivityReceiver, intentFilter)
+
         // Verificar la conexión a Internet
         if (!isInternetAvailable()) {
             showNoInternetDialog()
             disableUserInteractions()
         }
     }
+
     private fun disableUserInteractions() {
         window.setFlags(
             WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
             WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
         )
     }
-        private fun showNoInternetDialog() {
-            val builder = AlertDialog.Builder(this, R.style.CustomAlertDialog)
-            val inflater = layoutInflater
-            val dialogView = inflater.inflate(R.layout.custom_alert_dialog, null)
-            builder.setView(dialogView)
 
-            // Configurar elementos del AlertDialog personalizado
-            val titleTextView = dialogView.findViewById<TextView>(R.id.alertTitle)
-            val messageTextView = dialogView.findViewById<TextView>(R.id.alertMessage)
-            titleTextView.text = "Sin conexión a Internet"
-            messageTextView.text = "No se ha detectado conexión a Internet. Por favor, revisa tu conexión."
+    private fun showNoInternetDialog() {
+        val builder = AlertDialog.Builder(this, R.style.CustomAlertDialog)
+        val inflater = layoutInflater
+        val dialogView = inflater.inflate(R.layout.custom_alert_dialog, null)
+        builder.setView(dialogView)
 
-            builder.setPositiveButton("Aceptar") { dialog, _ ->
-                dialog.dismiss()
-            }
-            builder.setCancelable(false)
-            val alertDialog = builder.create()
-            alertDialog.show()
+        // Configurar elementos del AlertDialog personalizado
+        val titleTextView = dialogView.findViewById<TextView>(R.id.alertTitle)
+        val messageTextView = dialogView.findViewById<TextView>(R.id.alertMessage)
+        titleTextView.text = "Sin conexión a Internet"
+        messageTextView.text =
+            "No se ha detectado conexión a Internet. Por favor, revisa tu conexión."
+
+        builder.setPositiveButton("Aceptar") { dialog, _ ->
+            dialog.dismiss()
+        }
+        builder.setCancelable(false)
+        val alertDialog = builder.create()
+        alertDialog.show()
 
 
     }
@@ -71,11 +82,33 @@ class MainActivity : AppCompatActivity() {
         val activeNetwork = connectivityManager.activeNetwork ?: return false
         val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
 
-        // If we check only for "NET_CAPABILITY_INTERNET", we get "true" if we are connected to a wifi
-        // which has no access to the internet. "NET_CAPABILITY_VALIDATED" also verifies that we
-        // are online
+
         return capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
                 && capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+    }
+
+    private fun enableUserInteractions() {
+        window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+    }
+
+    inner class ConnectivityReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (isInternetAvailable()) {
+                Toast.makeText(context, "Conexión a Internet establecida", Toast.LENGTH_SHORT)
+                    .show()
+                enableUserInteractions()
+            }else{
+                showNoInternetDialog()
+                disableUserInteractions()
+            }
+        }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        // Desregistrar el receptor al destruir la actividad para evitar memory leaks
+        connectivityReceiver?.let {
+            unregisterReceiver(it)
+        }
     }
 
 
